357ea38023e9c0d3649edee5039e66d133ea0a3f
Gerardo López <gerardwjones@gmail.com>
2021-01-25 09:21:50 -0600

    array made with class

---
+++ denoScripts.ts
@@ -0,0 +1,12 @@
+
+class Player {
+    name: string
+    height: number
+    constructor(name: string, height: number) {
+        this.name = name
+        this.height = height
+    }
+}
+
+let player = new Player('luis', 45)
+
